f2f534ac56880a79256e36775ad23f9a1ff64036
Keepoccino <git@gietenbruch.net>
2020-04-18 13:01:16 +0200

    Basic Rock, that can purr

---
+++ Assets/MenuHandler.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuHandler : MonoBehaviour
+{
+    public GameObject Menu;
+    public GameObject OpenButton;
+
+    private void Awake()
+    {
+        Menu.SetActive(false);
+        OpenButton.SetActive(true);
+    }
+
+    public void OnMenuOpenClick()
+    {
+        Menu.SetActive(true);
+        OpenButton.SetActive(false);
+    }
+
+    public void OnMenuCloseClick()
+    {
+        Menu.SetActive(false);
+        OpenButton.SetActive(true);
+    }
+}
---
+++ Assets/RockAnimationController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RockAnimationController : MonoBehaviour
+{
+    public bool isPurring;
+
+    private float purr_intensity = 0.02f;
+    private float purr_frequency = 50;
+    private float purrOffset;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (isPurring)
+            purrOffset = Mathf.Sin(Time.time * purr_frequency) * purr_intensity;
+        else
+            purrOffset = 0;
+
+        transform.localPosition = new Vector3(purrOffset, 0);
+    }
+
+    void DoJump()
+    {
+    }
+}
