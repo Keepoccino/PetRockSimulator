49a4b779725c68becc387c6481b55476413e87ad
Keepoccino <git@gietenbruch.net>
2020-04-19 00:37:40 +0200

    Added Buttons to Action Menu and improved Watering Can

---
+++ Assets/UtilityController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UtilityController : MonoBehaviour
+{
+    public List<GameObject> UtilityPrefabs;
+
+    private GameObject currentUtility;
+
+    public void ChangeUtility(int num)
+    {
+
+        Destroy(currentUtility);
+        currentUtility = null;
+        if (num > 0)
+        {
+            currentUtility = Instantiate(UtilityPrefabs[num]);
+        }
+    }
+}
