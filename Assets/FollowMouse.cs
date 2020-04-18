89d3fed4084810a9ab7b3cb40834b8f274d74108
Keepoccino <git@gietenbruch.net>
2020-04-18 20:49:01 +0200

    Added WateringCan

--- Assets/FollowMouse.cs
+++ Assets/FollowMouse.cs
@@ -4,11 +4,9 @@ using UnityEngine;
 
 public class FollowMouse : MonoBehaviour
 {
-    public float dampTime = 0.15f;
     public float moveSpeed = 0.3f;
-    private Vector3 target;
 
-    private Vector3 velocity = Vector3.zero;
+    private Vector2 currentVelocity;
 
     private void Start()
     {
@@ -28,6 +26,7 @@ public class FollowMouse : MonoBehaviour
     void FixedUpdate()
     {
         var destination = GetDestination();
-        transform.position = Vector2.Lerp(transform.position, destination, moveSpeed);
+        //transform.position = Vector2.Lerp(transform.position, destination, moveSpeed);
+        transform.position = Vector2.SmoothDamp(transform.position, destination, ref currentVelocity, moveSpeed);
     }
 }
---
+++ Assets/WateringScript.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Unity.Mathematics;
+using UnityEngine;
+
+public class WateringScript : MonoBehaviour
+{
+    public float moveTime = 0.3f;
+    public float TurnDegrees = 45f;
+    public ParticleSystem WaterParticleSystem;
+
+    private float currentMoveVelocity;
+    private bool emmitWater;
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        var rotateToDegrees = 0f;
+        emmitWater = false;
+        if (Input.GetMouseButton(0))
+        {
+            rotateToDegrees = TurnDegrees;
+            if(Mathf.DeltaAngle(rotateToDegrees, transform.rotation.eulerAngles.z) < 5f){
+                emmitWater = true;
+            }
+        }
+        var em = WaterParticleSystem.emission;
+        em.enabled = emmitWater;
+        transform.rotation = Quaternion.AngleAxis(Mathf.SmoothDampAngle(transform.rotation.eulerAngles.z, rotateToDegrees, ref currentMoveVelocity, moveTime), Vector3.forward);
+    }
+}
