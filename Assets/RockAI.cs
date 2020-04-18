2ce0e74a2ebc6b8d86b321063c8e465cd1db760d
Keepoccino <git@gietenbruch.net>
2020-04-18 15:43:49 +0200

    Added stats for the rock

--- Assets/RockAI.cs
+++ Assets/RockAI.cs
@@ -4,6 +4,34 @@ using UnityEngine;
 
 public class RockAI : MonoBehaviour
 {
+    float secondsPerMinute = 60;
+
+
+    public StatController statSleepController;
+    public StatController statHungerController;
+    public StatController statWaterController;
+    public StatController statLoveController;
+    public StatController statHygieneController;
+    public StatController statGrowthController;
+
+
+    [SerializeField]
+    private float statSleep = 0.8f;
+    [SerializeField]
+    private float statHunger = 0.5f;
+    [SerializeField]
+    private float statWater = 0.5f;
+    [SerializeField]
+    private float statLove = 0.5f;
+    [SerializeField]
+    private float statHygiene = 0.5f;
+    [SerializeField]
+    private float statGrowth = 0.5f;
+
+    //Rates are per minute
+    private float statSleepRecovery = 0.20f;
+
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +41,18 @@ public class RockAI : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+
+        statSleep += statSleepRecovery * (Time.deltaTime / secondsPerMinute);
+
+
+        statSleep = Mathf.Clamp(statSleep, 0, 1);
+
+
+        statSleepController?.SetCurrentValue(statSleep);
+        statHungerController?.SetCurrentValue(statHunger);
+        statWaterController?.SetCurrentValue(statWater);
+        statLoveController?.SetCurrentValue(statLove);
+        statHygieneController?.SetCurrentValue(statHygiene);
+        statGrowthController?.SetCurrentValue(statGrowth);
     }
 }
---
+++ Assets/StatController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.UI;
+
+[ExecuteInEditMode]
+public class StatController : MonoBehaviour
+{
+    public RectTransform rectTransformProgress;
+    public Image progressBarImage;
+    public Text textObject;
+    public float progressBarMargin = 3;
+    public float currentValue = 0;
+    public float maxValue = 1;
+    public float maxWidthInner;
+    public Color Color;
+
+    private void Awake()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        textObject.color = Color;
+        progressBarImage.color = Color;
+        rectTransformProgress.sizeDelta = new Vector2(maxWidthInner * (currentValue / maxValue), rectTransformProgress.sizeDelta.y);
+    }
+
+    public void SetCurrentValue(float value)
+    {
+        currentValue = value;
+    }
+}
